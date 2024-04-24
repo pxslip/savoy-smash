0b258982c5ce420b5ef0833b72dc7605ee5cf1b4
Will K <dev@pxslip.com>
2024-04-24 04:05:40 +0000

    Tailwind stuff

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,15 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-} satisfies Config
-
+	content: [],
+	theme: {
+		extend: {
+			spacing: {
+				'8xl': '90rem',
+				'9xl': '100rem',
+				'10xl': '110rem',
+			},
+		},
+	},
+	plugins: [],
+} satisfies Config;
