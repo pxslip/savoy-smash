bd7cbb82d121d9c905199975c2bb4e22167e30b8
Will K <dev@pxslip.com>
2024-04-24 04:05:51 +0000

    Start making the layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,28 @@
 <template>
-  <div>
-    <slot />
-  </div>
+	<header class="flex justify-between border-b border-black">
+		<NuxtLink
+			to="/"
+			class="text-2xl py-4"
+		>
+			Savoy Smash
+		</NuxtLink>
+		<nav class="justify-end">
+			<menu class="h-full flex">
+				<li>Some Nav Item?</li>
+				<li>Site Search</li>
+			</menu>
+		</nav>
+	</header>
+	<div class="pt-4 max-w-8xl mx-auto">
+		<slot />
+	</div>
+	<footer></footer>
 </template>
 
-<script lang="ts" setup>
+<script lang="ts" setup></script>
 
-</script>
-
-<style>
-
+<style lang="css">
+	menu li {
+		@apply px-4 py-2 text-lg self-center;
+	}
 </style>
